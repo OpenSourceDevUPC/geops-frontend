b6f6a997f645be951364559af387b44177d028e7
Gilbert <gilberthuarcaya@gmail.com>
2025-10-02 15:14:26 -0500

    feat: add initial Angular application structure with configuration, routing, and components

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import {Home} from './shared/presentation/views/home/home';
+
+const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found')
+  .then(m => m.PageNotFound);
+const baseTitle = 'GeoPs';
+export const routes: Routes = [
+  { path: 'home', component: Home, title: `${baseTitle} - Home` },
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+];
