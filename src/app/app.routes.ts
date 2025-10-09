25339702ab68f7156183e21481cadbd26943093e
abigoe02 <u202318049@upc.edu.pe>
2025-10-09 01:21:31 -0500

    chore(config): update app configuration, index and global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,54 @@
 import { Routes } from '@angular/router';
-import {Home} from './shared/presentation/views/home/home';
+import {Layout} from './shared/presentation/components/layout/layout';
 
-const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found')
-  .then(m => m.PageNotFound);
-const baseTitle = 'GeoPs';
 export const routes: Routes = [
-  { path: 'home', component: Home, title: `${baseTitle} - Home` },
-  { path: '', redirectTo: '/home', pathMatch:'full' },
-  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./shared/presentation/views/home/home')
+            .then(m => m.Home),
+        title: 'GeoPs - Home'
+      },
+
+      {
+        path: 'ofertas',
+        loadComponent: () =>
+          import('./loyalty/presentation/views/ofertas/ofertas.component')
+            .then(m => m.OfertasComponent),
+        title: 'GeoPs - Ofertas'
+      },
+
+      {
+        path: 'categorias',
+        loadComponent: () =>
+          import('./loyalty/presentation/views/categorias/categorias.component')
+            .then(m => m.CategoriasComponent),
+        title: 'GeoPs - Categorías'
+      },
+
+      {
+        path: 'favoritos',
+        loadComponent: () =>
+          import('./loyalty/presentation/views/favoritos/favoritos.component')
+            .then(m => m.FavoritosComponent),
+        title: 'GeoPs - Favoritos'
+      },
+
+      {
+        path: 'mis-cupones',
+        loadComponent: () =>
+          import('./loyalty/presentation/views/mis-cupones/mis-cupones.component')
+            .then(m => m.MisCuponesComponent),
+        title: 'GeoPs - Mis Cupones'
+      },
+
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+    ],
+  },
+
+  { path: '**', redirectTo: 'home' },
 ];
