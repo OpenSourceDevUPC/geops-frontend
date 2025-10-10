3d37a4cac49ab2986d3c228c9aa20af990f8a6f9
IDymian <u202321941@upc.edu.pe>
2025-10-10 17:03:26 -0500

    docs(help): add JSDoc documentation to all help module files

--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelpCenterComponent } from './presentation/views/help-center/help-center.component';
 import { TranslateModule } from '@ngx-translate/core';
+/**
+ * Módulo principal para la funcionalidad de ayuda.
+ * Declara y exporta los componentes relacionados con el Help Center.
+ */
 
 @NgModule({
   imports: [
