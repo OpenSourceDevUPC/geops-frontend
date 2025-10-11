3f3687075a90b03e81f05defd233c6b8d8d7115d
Gilbert <gilberthuarcaya@gmail.com>
2025-10-10 21:05:26 -0500

    fix(environment): update API base URL for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'https://json-server-1-p24l.onrender.com',
   platformProviderSubscriptionsEndpointPath: '/subscriptions',
   platformProviderUserEndpointPath: '/users',
   platformProviderPaymentEndpointPath: '/payments',
